e3b40167375c5c26421043090d204652456fd5af
Bruna Barbosa <brunadnbarbosa@gmail.com>
2022-09-09 12:33:15 -0300

    Requisito 5

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import Game from './pages/Game';
 import Login from './pages/Login';
 
 export default function App() {
@@ -7,6 +8,7 @@ export default function App() {
     <div>
       <Switch>
         <Route exact path="/" component={ Login } />
+        <Route exact path="/game" component={ Game } />
       </Switch>
     </div>
   );
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { loginAction } from '../redux/actions';
 
 class Login extends Component {
   state = {
     player: '',
     playerEmail: '',
     isDisabled: true,
+    shouldRedirect: false,
   };
 
   handleChange = ({ target }) => {
@@ -21,8 +26,17 @@ class Login extends Component {
     });
   };
 
+  onButtonClick = () => {
+    const { player } = this.state;
+    const { logPlayer } = this.props;
+    logPlayer(player, playerEmail);
+    this.setState({
+      shouldRedirect: true,
+    });
+  };
+
   render() {
-    const { player, playerEmail, isDisabled } = this.state;
+    const { player, playerEmail, isDisabled, shouldRedirect } = this.state;
     return (
       <form>
         <label htmlFor="player">
@@ -51,12 +65,22 @@ class Login extends Component {
           type="button"
           disabled={ isDisabled }
           data-testid="btn-play"
+          onClick={ this.onButtonClick }
         >
           Play
         </button>
+        {shouldRedirect && <Redirect to="/game" />}
       </form>
     );
   }
 }
 
-export default Login;
+Login.propTypes = {
+  logPlayer: PropTypes.func.isRequired,
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  logPlayer: (player) => dispatch(loginAction(player, playerEmail)),
+});
+
+export default connect(null, mapDispatchToProps)(Login);
